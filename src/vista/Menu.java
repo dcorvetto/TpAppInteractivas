f60b6b6ca81233ea35ecef280d06407581690f04
Alan Vega <alanmvega@gmail.com>
2016-10-04 19:42:50 -0300

    tabla de vista de reclamos

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -1,4 +1,5 @@
 package vista;
+
 import controlador.Sistema;
 import negocio.EnumRoles;
 
@@ -7,53 +8,53 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 
-public class Menu extends javax.swing.JFrame {
-	
-	private static final long serialVersionUID = -7998003568770812583L;
-	private JMenuBar jMenuBar1;
-	private JMenuItem jMenuItem3;
-	private JMenu jMenu1;
-	private JMenuItem jMenuItem2;
-	private JMenuItem jMenuItem1;
-	private JMenu jMenu3;
-	private JMenu jMenu2;
-	
-	private Integer codigoUsuario;
-	
-	public Menu(Integer codigoUsuario) {
-		super();
-		this.codigoUsuario = codigoUsuario;
-		setResizable(false);
-		initGUI();
-	}
-	
-	private void initGUI() {
-		Collection<EnumRoles> roles = Sistema.getInstancia().rolesUsuario(codigoUsuario);
-		
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		getContentPane().setLayout(null);
-		this.setBounds(100, 100, 500, 500);
-		this.setTitle("Sistema de Administracion de Reclamos");
-		jMenuBar1 = new JMenuBar();
-		setJMenuBar(jMenuBar1);
-		jMenu2 = new JMenu();
-		jMenuBar1.add(jMenu2);
-		jMenu2.setText("Administracion");
-		jMenu3 = new JMenu();
-		jMenu2.add(jMenu3);
-		jMenu3.setText("Reclamos");
-		jMenu3.setBounds(39, 0, 57, 23);
-		jMenuItem1 = new JMenuItem();
-		if(roles.contains(EnumRoles.CALL_CENTER) || roles.contains(EnumRoles.CONSULTA) || roles.contains(EnumRoles.ADMINISTRACION)){
-			jMenu3.add(jMenuItem1);
-			jMenuItem1.setText("Ver Reclamos");
-			jMenuItem1.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent evt) 
-				{
-					JFrame vistaReclamos = new VistaReclamos(codigoUsuario);
-				}
-			});
-		}
+public class Menu extends JFrame {
+
+    private static final long serialVersionUID = -7998003568770812583L;
+    private JMenuBar jMenuBar1;
+    private JMenuItem jMenuItem3;
+    private JMenu jMenu1;
+    private JMenuItem jMenuItem2;
+    private JMenuItem jMenuItem1;
+    private JMenu jMenu3;
+    private JMenu jMenu2;
+
+    private Integer codigoUsuario;
+
+    public Menu(Integer codigoUsuario) {
+        super();
+        this.codigoUsuario = codigoUsuario;
+        setResizable(false);
+        initGUI();
+    }
+
+    private void initGUI() {
+        Collection<EnumRoles> roles = Sistema.getInstancia().rolesUsuario(codigoUsuario);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+        this.setBounds(100, 100, 500, 500);
+        this.setTitle("Sistema de Administracion de Reclamos");
+        jMenuBar1 = new JMenuBar();
+        setJMenuBar(jMenuBar1);
+        jMenu2 = new JMenu();
+        jMenuBar1.add(jMenu2);
+        jMenu2.setText("Administracion");
+        jMenu3 = new JMenu();
+        jMenu2.add(jMenu3);
+        jMenu3.setText("Reclamos");
+        jMenu3.setBounds(39, 0, 57, 23);
+        jMenuItem1 = new JMenuItem();
+        if (roles.contains(EnumRoles.CALL_CENTER) || roles.contains(EnumRoles.CONSULTA) || roles.contains(EnumRoles.ADMINISTRACION)) {
+            jMenu3.add(jMenuItem1);
+            jMenuItem1.setText("Ver Reclamos");
+            jMenuItem1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
+                    JFrame vistaReclamos = new VistaReclamos(codigoUsuario);
+                    vistaReclamos.setVisible(true);
+                }
+            });
+        }
 //		jMenuItem2 = new JMenuItem();
 //		jMenu3.add(jMenuItem2);
 //		jMenuItem2.setText("Tratamiento reclamos");
@@ -62,20 +63,19 @@ public class Menu extends javax.swing.JFrame {
 //			{
 //			}
 //		});
-		jMenu1 = new JMenu();
-		jMenuBar1.add(jMenu1);
-		jMenu1.setText("Salir del Sistema");
-		jMenuItem3 = new JMenuItem();
-		jMenu1.add(jMenuItem3);
-		jMenuItem3.setText("Salir");
-		jMenuItem3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) 
-			{
-				System.exit(0);
-			}
-		});
-		pack();
-		this.setSize(371, 235);
-	}
+        jMenu1 = new JMenu();
+        jMenuBar1.add(jMenu1);
+        jMenu1.setText("Salir del Sistema");
+        jMenuItem3 = new JMenuItem();
+        jMenu1.add(jMenuItem3);
+        jMenuItem3.setText("Salir");
+        jMenuItem3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                System.exit(0);
+            }
+        });
+        pack();
+        this.setSize(371, 235);
+    }
 
 }
--- src/vista/VistaReclamos.java
+++ src/vista/VistaReclamos.java
@@ -4,34 +4,45 @@ package vista;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.Vector;
 
 public class VistaReclamos extends JFrame {
 
-	private JPanel contentPane;
-	
-	private Integer codigoUsuario;
-	private JTable table;
+    private JPanel contentPane;
 
-	public VistaReclamos(Integer codigoUsuario) {
-		setTitle("Reclamos");
-		this.codigoUsuario = codigoUsuario;
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 425, 302);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		String[] nombresColumnas = {"Numero", "Detalle", "Estado", "Fecha"};
-		
-		table = new JTable(null, nombresColumnas);
-		table.setBounds(10, 189, 389, -155);
-		contentPane.add(table);
-		
-		JButton btnAgregar = new JButton("Agregar");
-		btnAgregar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnAgregar.setBounds(10, 216, 89, 23);
-		contentPane.add(btnAgregar);
-	}
+    private Integer codigoUsuario;
+    private JTable table;
+    JScrollPane jScrollPane;
+
+    public VistaReclamos(Integer codigoUsuario) {
+        setTitle("Reclamos");
+        this.codigoUsuario = codigoUsuario;
+
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        setBounds(100, 100, 425, 302);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        Vector<String> nombresColumnas = new Vector<>();
+        nombresColumnas.add("Numero");
+        nombresColumnas.add("Detalle");
+        nombresColumnas.add("Estado");
+        nombresColumnas.add("Fecha");
+
+        Vector<String> data = new Vector<>();
+
+        table = new JTable(data, nombresColumnas);
+        table.setBounds(0, 20, 389, 185);
+        table.setFillsViewportHeight(true);
+        jScrollPane = new JScrollPane(table);
+        jScrollPane.setBounds(10, 10, table.getWidth(), table.getHeight());
+        contentPane.add(jScrollPane);
+
+        JButton btnAgregar = new JButton("Agregar");
+        btnAgregar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        btnAgregar.setBounds(10, 216, 89, 23);
+        contentPane.add(btnAgregar);
+    }
 }

1abdbf5fddaa78847c537d6ea5938024dc429626
Alan Vega <alanmvega@gmail.com>
2016-10-03 23:39:04 -0300

    vista reclamos

---
+++ src/vista/VistaReclamos.java
@@ -0,0 +1,43 @@
+package vista;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.TableHeaderUI;
+import javax.swing.table.TableModel;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.Font;
+
+public class VistaReclamos extends JFrame {
+
+	private JPanel contentPane;
+	
+	private Integer codigoUsuario;
+	private JTable table;
+
+	public VistaReclamos(Integer codigoUsuario) {
+		setTitle("Reclamos");
+		this.codigoUsuario = codigoUsuario;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 425, 302);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		String[] nombresColumnas = {"Numero", "Detalle", "Estado", "Fecha"};
+		
+		table = new JTable(null, nombresColumnas);
+		table.setBounds(10, 189, 389, -155);
+		contentPane.add(table);
+		
+		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnAgregar.setBounds(10, 216, 89, 23);
+		contentPane.add(btnAgregar);
+	}
+}
